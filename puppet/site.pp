4905ced44c18ee35015931cfbd47f84630650fa7
Kevin Beam <kwbeam@gmail.com>
2015-06-16 10:46:05 -0600

    Strip out webapp and configuration the base VM

--- puppet/site.pp
+++ puppet/site.pp
@@ -1,51 +1,59 @@
 # Load modules and classes
 hiera_include('classes')
+include nsidc_miniconda
 
-class { 'nodejs':
-  version      => 'stable',
-  make_install => false,
-}
+if $environment != 'ci' {
 
-file { '/usr/bin/node':
-  ensure  => 'link',
-  target  => '/usr/local/node/node-default/bin/node',
-  require => Class['nodejs']
-}
-
-$cmd = 'sudo /usr/local/node/node-default/bin/npm install grunt-cli jspm -g'
-exec { 'install-node-deps':
-  command => $cmd,
-  path    => '/usr/bin',
-  require => Class['nodejs']
-}
-
-if $environment == 'ci' {
-
-  class { '::phantomjs':
-      package_version => '1.9.7',
-      package_update => true,
-      install_dir => '/usr/local/bin',
-      source_dir => '/opt',
-      timeout => 300
+  nsidc_miniconda::install { '/opt/miniconda':
+    version => '3.9.1',
+    build   => true
   }
 
-  # For acceptance tests
-  package { 'vnc4server': } ->
-  package { 'expect': } ->
-  exec { 'set_vnc_password':
-    path => '/usr/bin/',
-    command => 'sudo -i -u jenkins tr -dc A-Z < /dev/urandom | head -c 8 | /usr/bin/expect -c "set passwd [read stdin]; spawn sudo -i -u jenkins vncpasswd; expect \"Password:\"; send -- \"\$passwd\r\"; expect \"Verify:\"; send -- \"\$passwd\r\r\";exit;"'
+  nsidc_miniconda::config {
+    channels => ['https://conda.binstar.org/nsidc/channel/main', 'https://conda.binstar.org/nsidc/channel/dev']
   }
 
-} else {
+  class { 'nodejs':
+    version      => 'stable',
+    make_install => false,
+  }
+
+  file { '/usr/bin/node':
+    ensure  => 'link',
+    target  => '/usr/local/node/node-default/bin/node',
+    require => Class['nodejs']
+  }
+
+  file { '/usr/bin/npm':
+    ensure  => 'link',
+    target  => '/usr/local/node/node-default/bin/npm',
+    require => Class['nodejs']
+  }
+
+  $cmd = 'sudo npm install grunt-cli jspm -g'
+  exec { 'install-node-deps':
+    command => $cmd,
+    path    => '/usr/bin',
+    require => Class['nodejs']
+  }
 
   $hiera_project = hiera('project')
   $application_root = "/opt/${hiera_project}"
 
   class { 'nginx': }
 
+  # TODO: Fix the port, maybe
+  nginx::resource::upstream { 'icebridge-services':
+    members => ['localhost:5000'],
+  }
+
   nginx::resource::vhost { 'icebridge':
-    www_root => $application_root,
+    www_root => $application_root
+  }
+
+  nginx::resource::location { '/services':
+    location => '/services',
+    proxy => 'http://icebridge-services'
   }
 
   # remove default nginx config
