488ac937534b56774d076ca620f41a03e481757b
Kevin Beam <kwbeam@gmail.com>
2015-07-01 12:25:52 -0600

    Add docker-compose to VM provisioning

--- puppet/site.pp
+++ puppet/site.pp
@@ -2,3 +2,8 @@
 hiera_include('classes')
 
 include 'docker'
+
+exec { 'docker-compose':
+  command => 'curl -L https://github.com/docker/compose/releases/download/1.3.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose',
+  path => ['/bin', '/usr/bin']
+}
