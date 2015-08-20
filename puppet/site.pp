d15cde6e0e4c852e54104d105e4807b71299497d
Stuart Reed <stuart.reed@colorado.edu>
2015-08-20 10:39:44 -0600

    Add docker-compose up to CI. Add vagrant and jenkins users to docker group.

--- puppet/site.pp
+++ puppet/site.pp
@@ -1,7 +1,10 @@
 # Load modules and classes
 hiera_include('classes')
 
-include 'docker'
+class { 'docker':
+  version => '1.7.0',
+  docker_users => [ 'vagrant', 'jenkins' ],
+}
 
 exec { 'docker-compose':
   command => 'curl -L https://github.com/docker/compose/releases/download/1.3.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose',
