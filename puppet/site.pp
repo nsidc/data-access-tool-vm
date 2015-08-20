4b1454246822b995af5d672dfc8cca998b010c58
Stuart Reed <stuart.reed@colorado.edu>
2015-08-20 13:27:42 -0600

    Upgrade docker-compose in attempt to fix downloading from dockerhub

--- puppet/site.pp
+++ puppet/site.pp
@@ -7,6 +7,6 @@ class { 'docker':
 }
 
 exec { 'docker-compose':
-  command => 'curl -L https://github.com/docker/compose/releases/download/1.3.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose',
+  command => 'curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose',
   path => ['/bin', '/usr/bin']
 }
