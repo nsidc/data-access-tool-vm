b1a80e890180fd2cc283952e6b6495600dd5d585
Kevin Beam <kwbeam@gmail.com>
2015-08-21 17:58:45 -0600

    Fix SHARE_PATH and DEPLOYMENT_ENV usage
    1. Container code only needs to know about SHARE_PATH, which is set to
    /share/apps/icebridge-portal in the common docker-compose.
    2. Development mounts the ~/.icebridge-volume to the SHARE_PATH
    3. Production mounts /share/apps/icebridge-portal/${DEPLOYMENT_ENV} to
    the SHARE_PATH
    4. We only have to set SHARE_PATH on the command-line for the
    docker-compose up command
    5. Create the /share/apps/icebridge-portal/${DEPLOYMENT_ENV} when we
    provision a machine for a given environment

--- puppet/site.pp
+++ puppet/site.pp
@@ -19,3 +19,8 @@ exec { 'docker-compose':
   command => 'curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose',
   path => ['/bin', '/usr/bin']
 }
+
+file { 'app-share':
+  path  => "/share/apps/icebridge-portal/${environment}",
+  ensure => "directory"
+}
