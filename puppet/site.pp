1468775de6291d749c3925db89f16b4eb5fc9f59
Michael Brandt <michaelbrandt5@gmail.com>
2018-01-17 12:33:42 -0700

    install on dev: node, npm, jq, conda, docker-compose

--- puppet/site.pp
+++ puppet/site.pp
@@ -57,6 +57,23 @@ else {
   }
 }
 
+if $environment == 'dev' {
+  exec { 'setup node':
+    command => 'curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -',
+    path => '/usr/bin'
+  } ->
+  package { 'nodejs': } ->
+  package { 'npm': }
+
+  exec { 'install docker-compose':
+    command => 'curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose',
+    creates => '/usr/local/bin/docker-compose',
+    path => '/usr/bin'
+  }
+
+  package { 'jq': }
+}
+
 exec { 'swarm':
   command => 'docker swarm init --advertise-addr eth0:2377 --listen-addr eth0:2377 || true',
   path => ['/usr/bin', '/usr/sbin',]
