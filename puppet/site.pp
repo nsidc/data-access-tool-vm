8a958a0cb32e26b519878bca6acd36c9b61b5b76
Trey Stafford <trey.stafford@colorado.edu>
2025-03-19 10:03:23 -0600

    Add puppetry for non-dev setup

--- puppet/site.pp
+++ puppet/site.pp
@@ -1,18 +1,12 @@
 # Load modules and classes
 lookup('classes', {merge => unique}).include
 
-# NOTE: only dev is currently supported.
-
 class {'docker':
-  # TODO: update version
-  # version      => '5:26.1.1-1~ubuntu.22.04~jammy',
   docker_users => ['vagrant'],
 }
 
 class {'docker::compose':
   ensure  => 'present',
-  # TODO: update version
-  # version => '1.28.5',
 }
 
 file { 'envvars':
@@ -32,5 +26,50 @@ vcsrepo { 'clone data-access-tool-backend':
   source   => 'git@github.com:nsidc/data-access-tool-backend.git',
   owner    => 'vagrant',
   group    => 'vagrant',
-  revision => 'main',
+  # TODO: change this back to `main` once merged.
+  revision => 'dev-and-prod-server-settings',
+}
+
+if $::environment == 'dev' {
+  # Setup symlink for docker-compose dev
+  exec { 'setup backend docker-compose-dev':
+    command => 'ln -s docker-compose.dev.yml docker-compose.override.yml',
+    path => '/usr/bin/',
+    cwd    => '/home/vagrant/data-access-tool/data-access-tool-backend',
+    unless => 'test -f /home/vagrant/data-access-tool/data-access-tool-backend/docker-compose.override.yml',
+    require => [Vcsrepo['clone data-access-tool-backend']],
+  }
+
+  # Bring up the stack with docker compose up --detach.
+  # TODO: this does not work. The images get built but the stack is not up,
+  # despite the logs showing success...
+  exec { 'up-docker-stack':
+    command => 'bash -lc "nohup docker compose up --detach"',
+    path => '/usr/bin/',
+    cwd    => '/home/vagrant/data-access-tool/data-access-tool-backend',
+    user => 'vagrant',
+    require => [
+      Vcsrepo['clone data-access-tool-backend'],
+      Exec['setup backend docker-compose-dev'],
+      Class['docker'],
+      Class['docker::compose'],
+    ],
+  }
+} else {
+  # Bring up the stack with docker compose up --detach.
+  # TODO: this does not work. The images get built but the stack is not up,
+  # despite the logs showing success...
+  exec { 'up-docker-stack':
+    # TODO: remove build setep once images are published and we have
+    # version-driven releases
+    command => 'bash -lc "nohup docker compose up --detach"',
+    path => '/usr/bin/',
+    cwd    => '/home/vagrant/data-access-tool/data-access-tool-backend',
+    user => 'vagrant',
+    require => [
+      Vcsrepo['clone data-access-tool-backend'],
+      Class['docker'],
+      Class['docker::compose'],
+    ],
+  }
 }
